276473bf1023a1c41c9a00478b563526b262cd83
Deiadara <nikos@commonprefix.com>
2025-07-28 18:34:06 +0300

    fix: remove extra field

--- migrations/0004_payloads.sql
+++ migrations/0004_payloads.sql
@@ -1,5 +1,4 @@
 CREATE TABLE IF NOT EXISTS payloads (
     id	                TEXT NOT NULL PRIMARY KEY,
     payload	            TEXT NOT NULL,
-    created_at            TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
